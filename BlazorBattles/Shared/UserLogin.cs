d16dcddca54b59fad6185e27526cba16303f1316
SkyThunder <skysidtim@gmail.com>
2022-02-25 17:30:05 +0100

    add login form

---
+++ BlazorBattles/Shared/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBattles.Shared
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage ="Please, enter username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please, enter password")]
+        public string Password { get; set; }
+    }
+}

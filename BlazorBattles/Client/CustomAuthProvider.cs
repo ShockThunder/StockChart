36a01cac9737bdb6e7835e7570f7f730bb742568
SkyThunder <skysidtim@gmail.com>
2022-02-28 21:44:53 +0100

    add logout button. Add authorization state

--- BlazorBattles/Client/CustomAuthProvider.cs
+++ BlazorBattles/Client/CustomAuthProvider.cs
@@ -1,20 +1,34 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
 
 namespace BlazorBattles.Client
 {
     public class CustomAuthProvider : AuthenticationStateProvider
     {
-        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        public CustomAuthProvider(ILocalStorageService localStorageService)
         {
-            var identity = new ClaimsIdentity(new[]
-            {
-                new Claim(ClaimTypes.Name, "Vasya")
-            }, "test auth type");
+            _localStorageService = localStorageService;
+        }
 
-            var user = new ClaimsPrincipal(identity);
-            //return Task.FromResult(new AuthenticationState(user));
-            return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));
+        private readonly ILocalStorageService _localStorageService;
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var state = new AuthenticationState(new ClaimsPrincipal());
+            if (await _localStorageService.GetItemAsync<bool>("isAuthenticated"))
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, "Vasya")
+                }, "test auth type");
+
+                var user = new ClaimsPrincipal(identity);
+                state = new AuthenticationState(user);
+            }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+            return state;
+            
         }
     }
 }

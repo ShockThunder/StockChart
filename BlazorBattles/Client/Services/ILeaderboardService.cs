c9cf0cffc3aeaced3dd54ad81484b6e138dd6ed5
SkyThunder <skysidtim@gmail.com>
2022-03-07 12:48:04 +0100

    add leaderboard page

---
+++ BlazorBattles/Client/Services/ILeaderboardService.cs
@@ -0,0 +1,10 @@
+﻿using BlazorBattles.Shared;
+
+namespace BlazorBattles.Client.Services
+{
+    public interface ILeaderboardService
+    {
+        IList<UserStatistics> Leaderboard { get; set; }
+        Task GetLeaderboard();
+    }
+}
---
+++ BlazorBattles/Client/Services/LeaderboardService.cs
@@ -0,0 +1,21 @@
+﻿using BlazorBattles.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorBattles.Client.Services
+{
+    public class LeaderboardService : ILeaderboardService
+    {
+        private readonly HttpClient _httpClient;
+
+        public LeaderboardService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public IList<UserStatistics> Leaderboard { get; set; }
+
+        public async Task GetLeaderboard()
+        {
+            Leaderboard = await _httpClient.GetFromJsonAsync<IList<UserStatistics>>("api/user/leaderboard");
+        }
+    }
+}
